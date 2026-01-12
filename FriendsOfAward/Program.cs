b9c29657ca0fba4beb2e1493d990ee8fe91f6631
PaulTh <paul.thalhammer@yahoo.com>
2026-01-12 09:13:19 +0100

    Login Page implementier und Programm.cs builder hinzugefügt (DPL aukommentiert, da die Date fehler wirft)

--- FriendsOfAward/Program.cs
+++ FriendsOfAward/Program.cs
@@ -1,4 +1,6 @@
+using FriendsOfAward;
 using FriendsOfAward.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<MyCustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+  provider.GetRequiredService<MyCustomAuthStateProvider>());
 
 var app = builder.Build();
 
