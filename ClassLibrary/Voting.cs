106bccd8198ef55595ece6c9cce7f0b0c9c5e27d
m.schmid <m.schmid@htlvb.at>
2026-02-03 11:53:11 +0100

    qr code validation check

--- ClassLibrary/Voting.cs
+++ ClassLibrary/Voting.cs
@@ -21,6 +21,24 @@
         DaOther5 = o5;
     }
 
+    /// <summary>
+    /// Checks if a QR code exists in the database
+    /// </summary>
+    /// <param name="qr">The QR code to check</param>
+    /// <returns>True if the QR code exists, false otherwise</returns>
+    public static bool QrCodeExists(string qr)
+    {
+        try
+        {
+            object? qrExists = DbWrapper.Wrapper.RunQueryScalar($"SELECT COUNT(*) FROM FoA_QrCodes WHERE QrID = '{qr}'");
+            return qrExists != null && Convert.ToInt32(qrExists) > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
     ///<summary>
     ///(string qrCode, Liste mit id von Diplomarbeiten (erster wert=top-fav)
     ///</summary>
@@ -28,7 +46,13 @@
     {
         try
         {
-            int num = DbWrapper.Wrapper.RunNonQuery($"INSERT INTO Vote VALUES (NULL, {qr}, {titel[0]}, {titel[1]}, {titel[2]}, {titel[3]},{titel[4]};)");
+            bool validQrCode = QrCodeExists(qr);
+            if (!validQrCode)
+            {
+                return "Ungültiger User code";
+            }
+
+            int num = DbWrapper.Wrapper.RunNonQuery($"INSERT INTO Vote VALUES (NULL, '{qr}', {titel[0]}, {titel[1]}, {titel[2]}, {titel[3]}, {titel[4]})");
             if (num != 1) return $"Ein Fehler ist aufgetreten! Gespeicherte Votes: {num}";
             return "";
         }
