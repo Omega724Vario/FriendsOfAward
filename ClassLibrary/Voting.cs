e912ef1bc337788f7b87c8a344146f7e37bc5b54
AlexisCrain <153603776+AlexisCrain@users.noreply.github.com>
2026-02-24 12:04:39 +0100

    änderungen Ergebnisdatei

--- ClassLibrary/Voting.cs
+++ ClassLibrary/Voting.cs
@@ -1,4 +1,7 @@
-﻿public class Vote
+﻿using ClosedXML.Excel;
+using System.Data;
+
+public class Vote
 {
     int VotingId { get; }
     string QrId { get; }
@@ -36,5 +39,63 @@
         {
             return $"{ex}";
         }
+    }
+
+    public static List<(DA, int)> ShowVotings(List<(DA, int count)> voting)
+    {
+        //Ergebnisse vom Voting speichern
+
+        string sql = $"SELECT d.DaId, d.Titel, d.Schueler," +
+        "COUNT(v.DaId) AS vote_count FROM foa_da AS d INNER JOIN foa_voting AS v ON d.DaId = v.DaId GROUP BY d.DaId, d.Titel, d.Schueler;";
+        //ID | Titel | Schueler | Count
+        DataTable da = DbWrapper.Wrapper.RunQuery(sql);
+        DA result;
+        //Schleife für jede DA in Db, pro durchgang wird
+        foreach (DataRow row in da.Rows)
+        {
+            result = new DA((int)row[0], (string)row[1], (string)row[2]);
+            int count = Convert.ToInt32(row[3]);
+            voting.Add((result, count));
+        }
+        return voting; 
+    }
+
+    public static byte[] CreateExcelFile(List<(DA, int count)> voting)
+    {
+        XLWorkbook? workbook = new XLWorkbook();
+
+        voting = voting
+            .OrderByDescending(v => v.count)
+            .ToList();
+
+        // Referenz: https://www.youtube.com/watch?v=3DIKSjQMc5U
+        IXLWorksheet worksheet = workbook.Worksheets.Add("FoA_Ergebnisse");
+        worksheet.Cell("A1").Value = "Nr";
+        worksheet.Cell("B1").Value = "Titel";
+        worksheet.Cell("C1").Value = "Schüler";
+        worksheet.Cell("D1").Value = "Count";
+
+        int nr = 2;
+        foreach ((DA, int) v in voting)
+        {
+            worksheet.Row(nr).Cell("A").Value = v.Item1.ID;
+            worksheet.Row(nr).Cell("B").Value = v.Item1.Titel;
+            worksheet.Row(nr).Cell("C").Value = v.Item1.Schueler;
+            worksheet.Row(nr).Cell("D").Value = v.Item2;
+            nr++;
+        }
+
+        IXLRange? range = worksheet.Range($"A1:D{voting.Count + 1}");
+        IXLTable table = range.CreateTable();
+        table.Theme = XLTableTheme.TableStyleLight10;
+
+        worksheet.Cell($"A{voting.Count + 1}").Value = "Total Votes: ";
+        worksheet.Cell($"D{voting.Count + 1}").FormulaA1 = $"SUM(D2:D{voting.Count + 1})";
+
+        worksheet.Row(voting.Count + 1).Style.Font.Bold = true;
+
+        using var stream = new MemoryStream();
+        workbook.SaveAs(stream);
+        return stream.ToArray();
     }
 }
